0c4c17b6e4a1ab1d9f6590c974f86e0be2c4eee1
Alois Cochard <alois.cochard@gmail.com>
2014-07-02 23:58:49 +0100

    Add support of cabal sandbox 'add-source' as workspace dependencies.
    This commit fix: https://github.com/aloiscochard/codex/issues/1

--- src/Codex/Project.hs
+++ src/Codex/Project.hs
@@ -2,6 +2,7 @@ module Codex.Project where
 
 import Control.Exception (try, SomeException)
 import Data.Functor
+import Data.Function
 import Data.Maybe
 import Data.String.Utils
 import Data.Traversable (traverse)
@@ -26,7 +27,7 @@ import qualified Data.Map as Map
 newtype Workspace = Workspace [WorkspaceProject]
   deriving (Eq, Show)
 
-data WorkspaceProject = WorkspaceProject PackageIdentifier FilePath
+data WorkspaceProject = WorkspaceProject { workspaceProjectIdentifier :: PackageIdentifier, workspaceProjectPath :: FilePath }
   deriving (Eq, Show)
 
 type ProjectDependencies = (PackageIdentifier, [PackageIdentifier], [WorkspaceProject])
@@ -49,30 +50,21 @@ findPackageDescription root = do
 resolveCurrentProjectDependencies :: IO ProjectDependencies
 resolveCurrentProjectDependencies = do
   ws <- getWorkspace ".."
-  resolveProjectDependenciesWithWorkspace ws "."
+  resolveProjectDependencies ws "."
 
 -- TODO Optimize
-resolveProjectDependenciesWithWorkspace :: Workspace -> FilePath -> IO ProjectDependencies
-resolveProjectDependenciesWithWorkspace ws root = do
+resolveProjectDependencies :: Workspace -> FilePath -> IO ProjectDependencies
+resolveProjectDependencies ws root = do
   pd <- maybe (error "No cabal file found.") id <$> findPackageDescription root
-  xs <- resolveProjectDependencies root pd
-  let wsds = List.filter (shouldOverride xs) $ resolveWorkspaceDependencies ws pd
-  let pjds = List.filter (\x -> List.notElem (pkgName x) $ fmap (\(WorkspaceProject x _) -> pkgName x) wsds) xs
+  xs <- resolvePackageDependencies root pd
+  ys <- resolveSandboxDependencies root
+  let zs   = resolveWorkspaceDependencies ws pd
+  let wsds = List.filter (shouldOverride xs) $ List.nubBy (on (==) prjId) $ concat [ys, zs]
+  let pjds = List.filter (\x -> List.notElem (pkgName x) $ fmap prjId wsds) xs
   return (identifier pd, pjds, wsds) where
     shouldOverride xs (WorkspaceProject x _) =
       maybe True (\y -> pkgVersion x >= pkgVersion y) $ List.find (\y -> pkgName x == pkgName y) xs
-
-resolveProjectDependencies :: FilePath -> GenericPackageDescription -> IO [PackageIdentifier]
-resolveProjectDependencies root pd = do
-  xs <- either (fallback pd) return =<< resolveInstalledDependencies root
-  return xs where
-    fallback pd e = do
-      putStrLn $ concat ["cabal: ", show e]
-      putStrLn "codex: *warning* falling back on dependency resolution using hackage"
-      resolveWithHackage pd
-    resolveWithHackage pd = do
-      db <- readHackage
-      return $ identifier <$> resolveHackageDependencies db pd
+    prjId = pkgName . workspaceProjectIdentifier
 
 resolveInstalledDependencies :: FilePath -> IO (Either SomeException [PackageIdentifier])
 resolveInstalledDependencies root = try $ do
@@ -81,7 +73,8 @@ resolveInstalledDependencies root = try $ do
       ipkgs = installedPkgs lbi
       clbis = snd <$> allComponentsInBuildOrder lbi
       pkgs  = componentPackageDeps =<< clbis
-      xs = fmap sourcePackageId $ (maybeToList . lookupInstalledPackageId ipkgs) =<< fmap fst pkgs
+      ys = (maybeToList . lookupInstalledPackageId ipkgs) =<< fmap fst pkgs
+      xs = fmap sourcePackageId $ ys
   return xs where
     distPref = root </> "dist"
 
@@ -92,10 +85,42 @@ resolveHackageDependencies db pd = maybeToList . resolveDependency db =<< allDep
     latest <- List.find (\x -> withinRange x versionRange) $ List.reverse $ List.sort $ Map.keys pdsByVersion
     Map.lookup latest pdsByVersion
 
+resolvePackageDependencies :: FilePath -> GenericPackageDescription -> IO [PackageIdentifier]
+resolvePackageDependencies root pd = do
+  xs <- either (fallback pd) return =<< resolveInstalledDependencies root
+  return xs where
+    fallback pd e = do
+      putStrLn $ concat ["cabal: ", show e]
+      putStrLn "codex: *warning* falling back on dependency resolution using hackage"
+      resolveWithHackage pd
+    resolveWithHackage pd = do
+      db <- readHackage
+      return $ identifier <$> resolveHackageDependencies db pd
+
+resolveSandboxDependencies :: FilePath -> IO [WorkspaceProject]
+resolveSandboxDependencies root = do
+  fileExists  <- doesFileExist sourcesFile
+  if fileExists then readSources else return [] where
+    readSources = do
+      fileContent <- readFile sourcesFile
+      xs <- traverse readWorkspaceProject $ projects fileContent
+      return $ xs >>= maybeToList where
+        projects :: String -> [FilePath]
+        projects x = sources x >>= (\x -> fmap fst $ snd x)
+        sources :: String -> [(String, [(FilePath, Int)])]
+        sources x = read x
+    sourcesFile = root </> ".cabal-sandbox" </> "add-source-timestamps"
+
+
 resolveWorkspaceDependencies :: Workspace -> GenericPackageDescription -> [WorkspaceProject]
 resolveWorkspaceDependencies (Workspace ws) pd = maybeToList . resolveDependency =<< allDependencies pd where
   resolveDependency (Dependency name versionRange) =
     List.find (\(WorkspaceProject (PackageIdentifier n v) _) -> n == name && withinRange v versionRange) ws
+
+readWorkspaceProject :: FilePath -> IO (Maybe WorkspaceProject)
+readWorkspaceProject path = do
+  pd <- findPackageDescription path
+  return $ fmap (\x -> WorkspaceProject (identifier x) path) pd
 
 getWorkspace :: FilePath -> IO Workspace
 getWorkspace _root = do
@@ -105,10 +130,7 @@ getWorkspace _root = do
   return . Workspace $ ys >>= maybeToList where
     find path = do
       isDirectory <- doesDirectoryExist path
-      if isDirectory then do
-        pd <- findPackageDescription path
-        return $ fmap (\x -> WorkspaceProject (identifier x) path) pd
-      else return Nothing
+      if isDirectory then readWorkspaceProject path else return Nothing
     listDirectory fp = do
       xs <- getDirectoryContents fp
       return . fmap (fp </>) $ filter (not . startswith ".") xs
