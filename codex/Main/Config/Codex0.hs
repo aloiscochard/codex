2210271a1b19cd39b6c610f9b4a831ca414e60a6
Eyal Lotem <eyal.lotem@gmail.com>
2014-12-05 22:55:52 +0200

    Config: Share migration code (in preparation of adding another config version).
      * Each Codex version module responsible for its own warning and migration

--- codex/Main/Config/Codex0.hs
+++ codex/Main/Config/Codex0.hs
@@ -4,9 +4,16 @@ module Main.Config.Codex0 where
 import Data.Yaml
 import GHC.Generics
 
+import qualified Codex as New
+
 data Codex = Codex { hackagePath :: FilePath, tagsCmd :: String }
   deriving Generic
 
 instance ToJSON Codex
 instance FromJSON Codex
 
+migrateWarn :: IO ()
+migrateWarn = return ()
+
+migrate :: Codex -> New.Codex
+migrate cx = New.Codex True (hackagePath cx) (tagsCmd cx) True True
