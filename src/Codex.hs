6ef7ba36109ce7c24e7f5caba0c0643cbe118e3a
Alois Cochard <alois.cochard@gmail.com>
2015-05-21 15:03:15 +0200

    Revert removal of `Control.Applicative` and `Data.Traversable` import.
    Only if `base` < 4.8.

--- src/Codex.hs
+++ src/Codex.hs
@@ -1,4 +1,10 @@
+{-# LANGUAGE CPP #-}
 module Codex (Codex(..), defaultTagsFileName, Verbosity, module Codex) where
+
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative ((<$>))
+import Data.Traversable (traverse)
+#endif
 
 import Control.Exception (try, SomeException)
 import Control.Lens ((^.))
