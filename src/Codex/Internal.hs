42e2cea293c6d0726546a65e4f143274f6319e14
Alois Cochard <alois.cochard@gmail.com>
2015-10-22 00:00:54 +0200

    Fix import of `Applicative.(<$>)` under GHC 7.8.

--- src/Codex/Internal.hs
+++ src/Codex/Internal.hs
@@ -1,6 +1,11 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE StandaloneDeriving #-}
 module Codex.Internal where
+
+#if !MIN_VERSION_base(4,8,0)
+import Control.Applicative ((<$>))
+#endif
 
 import Data.Char (isSpace)
 import Data.Yaml
